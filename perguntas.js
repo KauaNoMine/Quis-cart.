f9b1616791c6d7798b09868300393bd3b260c276
kaique Proença <kaique.37.p@gmail.com>
2024-10-23 11:35:08 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,21 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matematica',
+    'Como calcular a area de um triangolo retangolo?',
+    'Area do triangulo retangulo e sua altura veses a largura depois devedido por dois (Altura X Largura)/2'
+)
+
+criaCartao(
+    'Lingua Portuquesa',
+    'Quais foram as geração Romantismo?',
+    'Primera Geração foi Nacionalista e Indionista, Segunda Outra Romatismo e a Teceira é Geração Romatica'
+)
